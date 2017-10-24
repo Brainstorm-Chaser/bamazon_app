c249c47606ea85f36517ab8df6f0487959221db3
Mike Murphy <mike@brainstormchaser.com>
2017-10-23 18:34:57 -0700

    before math function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,23 +25,25 @@ connection.connect(function(err) {
 
 function displayTable() {
   connection.query("SELECT * FROM items", function(err, res) {
+    // var stockQuantity = res[item].stock_quantity
     if (err){
       console.log(error)
    };
      console.log("")
      console.log("")
-     console.log(table.print(res));   
-    selectItem();
+     console.log(table.print(res));
   })
 }
 
+updateItem();
 
-function selectItem() {
+
+
+function updateItem() {
+  
   // query the database for all items being auctioned
   connection.query("SELECT * FROM items", function(err, res) {
     if (err) throw err;
-    
- 
     inquirer
       .prompt([
         {
@@ -56,8 +58,41 @@ function selectItem() {
           },
           message: "What item would you like to purchase?"
         },
+        {
+          name: "amount",
+          type: "input",        
+          message: "How many would you like?"
+        }
+
         ]).then(function(answer) {
-          console.log(answer.product_name);
+          // var stockTotal = parseInt(stock_quantity) - parseInt(answer.amount)
+          // console.log(stockTotal)
+
+          connection.query(
+            "UPDATE items SET ? WHERE ?",
+            [
+              {
+                stock_quantity: answer.amount
+              },
+              {
+                item_id: answer.choice
+              }
+            ],
+            function(error) {
+              // console.log(answer.amount)
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              // start();
+            })
+          // var productName = answer.product_name
+          // console.log(productName);
+          // var stockQuantity = (answer.stock_quantity)
+          // console.log(stockQuantity)
+          // // var answer = answer.amount
+          // // console.log(answer)
+          // return answer.amount
+
+          
        })
       })
     };
